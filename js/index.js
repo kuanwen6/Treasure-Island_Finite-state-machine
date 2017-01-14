bcd133c42b1f7d7a80fff6124918acf2b90457a7
Kuan-Wen Liu <j70915@gmail.com>
2017-01-15 01:02:29 +0800

    second-edit version

--- js/index.js
+++ js/index.js
@@ -1,27 +1,45 @@
 function restart(){
     console.log("restart");
+    $("#arrow1-2a").css("visibility","hidden");
+    document.getElementById("is1A").style.display="inherit"
+    document.getElementById("is1B").style.display="inherit"
+    path_click(document.getElementById("is1A"),"is1A","A")  //flag restart
+    path_click(document.getElementById("is1B"),"is1B","B")
+    document.getElementById("boat").style.left=180   //boat restart
+    document.getElementById("boat").style.top=310
+    for(var i=2;i<6;i++){
+        document.getElementById("is"+i+"A").style.display="none"
+        document.getElementById("is"+i+"B").style.display="none"
+    }
+    document.getElementById("description").click(); //modal description
+
+    initial_tag=0
+    intro()
+    $("#arr1-2").css("visibility","hidden");
+    $("#arr1-3").css("visibility","hidden");
+    $("#arr2-1").css("visibility","hidden");
+    $("#arr2-3").css("visibility","hidden");
+    $("#arr3-2").css("visibility","hidden");
+    $("#arr3-4").css("visibility","hidden");
+    $("#arr4-5").css("visibility","hidden");
+    $("#arr4-1").css("visibility","hidden");
+    $("#arr5-3").css("visibility","hidden");
+    $("#arr5-2").css("visibility","hidden");
 }
 
 function move(a,b){			//move from island a to island b
 	console.log("move from "+a+" to "+b);
 
-	if(a==1){
-		var rect = document.getElementById("island1")
-	}
-	else{
-		var rect = document.getElementById("island2")
-	}
-	console.log(rect.offsetLeft, rect.offsetTop)
-
-	x1=document.getElementById("island1")
-	x2=document.getElementById("island2")
-	xx1=x1.offsetLeft
-	y1=x1.offsetTop
-	xx2=x2.offsetLeft
-	y2=x2.offsetTop
-
-
-
+    if(initial_tag==1){
+        initial_tag++
+        console.log("sencond step intro")
+        $(".intro_text").css("display","none");
+        $("#red-a").css("display","none");
+        $("#yellow-a").css("display","none");
+        $(".intro_text_s").css("display","inherit");
+    }else{ //cancel
+        intro_no(b);
+    }
 
     var startX,startY,endX,endY,angle_start,angle_end,length,delay=2000
 
@@ -130,14 +148,16 @@ function move(a,b){			//move from island a to island b
         default:
             console.log("error path")
     }
-
-
+    document.getElementById("is"+a+"A").onclick=null  //lock the onclick event
+    document.getElementById("is"+a+"B").onclick=null
     setTimeout(function() {
         document.getElementById("is"+a+"A").style.display="none"
         document.getElementById("is"+a+"B").style.display="none"
         document.getElementById("is"+b+"A").style.display="inherit"
         document.getElementById("is"+b+"B").style.display="inherit"
-        $("#arrow1-2a").css("visibility","visible");
+        path_click(document.getElementById("is"+b+"A"),"is"+b+"A","A")  //unlock the destinction onclick event
+        path_click(document.getElementById("is"+b+"B"),"is"+b+"B","B")
+        $("#arr"+a+"-"+b).css("visibility","visible");
     }, delay);
 
 
@@ -175,14 +195,137 @@ function unhover(a){
 	$(".text").css("visibility","hidden");
 }
 
+function flag_hover(a,id){
+    if(initial_tag > 1){
+        if(a=="A"){
+            $(".flag_text").html("A");
+            $(".flag_text").css("color","red")
+            console.log("aaa")
+        }else{
+            $(".flag_text").html("B");
+            $(".flag_text").css("color","yellow")
+        }
+    }else{
+        $(".flag_text").html("");
+    }
+    console.log("a")
+
+    $(".flag_text").css("visibility","visible");
+    $(".flag_text").css("left",document.getElementById(id).offsetLeft+30);  // width + 10
+    $(".flag_text").css("top",document.getElementById(id).offsetTop-42);  // height -120
+
+}
+function flag_unhover(){
+    $(".flag_text").css("visibility","hidden");
+}
+
 $(document).ready(function(){
 	console.log("start");
-	//document.getElementById("description").click(); //modal description
-
+    intro()
+	document.getElementById("description").click(); //modal description
 });
 
-var island_name =["新手島","神燈島","中華島","龍之島","金銀島"]
+var initial_tag=0
 
+function intro(){
+    initial_tag=1
+    $(".intro_text").css("display","inherit");
+    $("#red-a").css("display","inherit");
+    $("#yellow-a").css("display","inherit");
+    $("#pirate_head").css("display","inherit");
+    $("#curve").css("display","inherit");
+    $(".intro_text_s").css("display","none");
+    $("#t1").html("哈哈!就是這樣");
+    $("#t2").html("加油吧!別迷路了!");
+}
+
+function intro_no(island){
+    $("#red-a").css("display","none");
+    $("#yellow-a").css("display","none");
+    $(".intro_text").css("display","none");
+    switch(island){
+        case 1:
+            $("#t1").html(".....................");
+            $("#t2").html("怎麼又回到原點了!?");
+            break;
+        case 2:
+            $("#t1").html("啊啊啊~好多沙啊");
+            $("#t2").html("好燙好燙~~~~");
+            break;
+        case 3:
+            $("#t1").html("&nbsp&nbsp&nbsp&nbsp台灣中國!");
+            $("#t2").html("&nbsp&nbsp&nbsp&nbsp一邊一國!");
+            break;
+        case 4:
+            $("#t1").html("......龍.....龍.....??");
+            $("#t2").html("我要成為訓龍高手!");
+            break;
+        case 5:
+            $("#t1").html("太神啦!感謝凱瑞");
+            $("#t2").html("寶藏都是我的了哈哈");
+            break;
+    }
+}
+
+function path_click(img,id,a){
+    img.onclick = function(){
+        if(id=="is1A"){
+            move(1,2)
+            boat_invert(0)
+        }else if(id=="is2A"){
+            move(2,3)
+            boat_invert(0)
+        }else if(id=="is3A"){
+            move(3,4)
+            boat_invert(0)
+        }else if(id=="is4A"){
+            move(4,5)
+            boat_invert(1)
+        }else if(id=="is5A"){
+            move(5,3)
+            boat_invert(1)
+        }else if(id=="is1B"){
+            move(1,3)
+            boat_invert(0)
+        }else if(id=="is2B"){
+            move(2,1)
+            boat_invert(1)
+        }else if(id=="is3B"){
+            move(3,2)
+            boat_invert(0)
+        }else if(id=="is4B"){
+            move(4,1)
+            boat_invert(1)
+        }else{
+            move(5,2)
+            boat_invert(1)
+        }
+    };
+    img.onmouseover=function(){
+        flag_hover(a,id)
+    }
+    img.onmouseout=function(){
+        flag_unhover();
+    }
+}
+
+function boat_invert(a){
+    if(a==1)
+    {
+        document.getElementById("boat").src="img/ship_invert.png";
+    }else{
+        document.getElementById("boat").src="img/ship.png";
+    }
+
+}
+
+function advance(){
+    $(".map").css("display","none");
+    $(".advance").append('<img src="img/FSM.png"')
+}
+
+
+var island_name =["新手島","神燈島","中華島","龍之島","金銀島"]
 
 for(var i=1;i<6;i++)  // A flag initial
 {
@@ -201,22 +344,10 @@ for(var i=1;i<6;i++)  // A flag initial
     img.style.top = y+70
     img.style.left= x-10
 
-    img.onclick = function(){
-        if(this.id=="is1A"){
-            move(1,2)
-        }else if(this.id=="is2A"){
-            move(2,3)
-        }else if(this.id=="is3A"){
-            move(3,4)
-        }else if(this.id=="is4A"){
-            move(4,5)
-        }else{
-            move(5,3)
-        }
-    };
-
     var src = document.getElementById("map");
     src.appendChild(img);
+
+    path_click(img,img.id,"A")
 }
 
 for(var i=1;i<6;i++)  // B flag initial
@@ -236,23 +367,12 @@ for(var i=1;i<6;i++)  // B flag initial
     img.style.top = y+70
     img.style.left= x+170
 
-    img.onclick = function(){
-        if(this.id=="is1B"){
-            move(1,3)
-        }else if(this.id=="is2B"){
-            move(2,1)
-        }else if(this.id=="is3B"){
-            move(3,2)
-        }else if(this.id=="is4B"){
-            move(4,1)
-        }else{
-            move(5,2)
-        }
-    };
-
     var src = document.getElementById("map");
     src.appendChild(img);
+
+    path_click(img,img.id,"B")
 }
 
 document.getElementById("is1A").style.display="inherit"
 document.getElementById("is1B").style.display="inherit"
+intro()
